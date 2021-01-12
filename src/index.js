f010747b4a555ed5a126e66a864151af1eb2f963
Artem <64410376+Artiss90@users.noreply.github.com>
2021-01-12 23:32:59 +0200

    Event & hosting

--- src/index.js
+++ src/index.js
@@ -1,12 +1,141 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import 'modern-normalize/modern-normalize.css';
 import './index.css';
-import App from './App';
+// import App from './App';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root'),
+// );
+
+class Counter extends Component {
+  // ******************************
+  /**static defaultProps = {
+    step: 1,
+  };
+   * handleIncrement = evt => {
+ console.log('Increment button was clicked!', evt); // работает
+ console.log('this.props: ', this.props); // работает
+};
+
+handleDecrement = evt => {
+  console.log('Decrement button was clicked!', evt); // работает
+  console.log('this.props: ', this.props); // работает
+}; */
+  /**заменяем на нижний код */
+  // *******************************
+  static defaultProps = {
+    step: 1,
+    initialValue: 0,
+  };
+
+  state = {
+    value: this.props.initialValue,
+  };
+  handleIncrement = () => {
+    this.setState((state, props) => ({
+      value: state.value + props.step,
+    }));
+  };
+
+  handleDecrement = () => {
+    this.setState((state, props) => ({
+      value: state.value - props.step,
+    }));
+  };
+  render() {
+    const { step } = this.props;
+
+    return (
+      <div>
+        <span>{this.state.value}</span>
+        <button type="button" onClick={this.handleIncrement}>
+          Increment by {step}
+        </button>
+        <button type="button" onClick={this.handleDecrement}>
+          Decrement by {step}
+        </button>
+      </div>
+    );
+  }
+}
+
+// ReactDOM.render(<Counter step={5} />, document.getElementById('root'));
+
+class Toggle extends Component {
+  state = { isOpen: false };
+  /**show = () => this.setState({ isOpen: true });
+
+  hide = () => this.setState({ isOpen: false });
+
+  render() {
+    const { isOpen } = this.state;
+    const { children } = this.props;
+
+    return (
+      <>
+        <button onClick={this.show}>Show</button>
+        <button onClick={this.hide}>Hide</button>
+        {isOpen && children}
+      </>
+    );
+  } */
+
+  /** верхний код заменяем*/
+  toggle = () => {
+    this.setState(state => ({ isOpen: !state.isOpen }));
+  };
+
+  render() {
+    const { isOpen } = this.state;
+    const { children } = this.props;
+
+    return (
+      <div>
+        <button onClick={this.toggle}>{isOpen ? 'Hide' : 'Show'}</button>
+        {isOpen && children}
+      </div>
+    );
+  }
+}
+
+const Button = ({ changeMessage, label }) => (
+  <button type="button" onClick={changeMessage}>
+    {label}
+  </button>
+);
+
+class AppTest extends Component {
+  state = {
+    message: new Date().toLocaleTimeString(),
+  };
+
+  // Метод который будем передавать в Button для вызова при клике
+  updateMessage = evt => {
+    console.log(evt); // Доступен объект события
+
+    this.setState({
+      message: new Date().toLocaleTimeString(),
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <span>{this.state.message}</span>
+        <Button label="Change message" changeMessage={this.updateMessage} />
+      </>
+    );
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Toggle>
+    <Counter step={5} />
+    <AppTest />
+  </Toggle>,
   document.getElementById('root'),
 );
