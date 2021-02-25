4ce6b4e96cbfdf5fc1f9e0e252020798d560b549
Artem <64410376+Artiss90@users.noreply.github.com>
2021-02-25 22:56:23 +0200

    Функция createReducer()

--- src/redux/timer/timerReducer.js
+++ src/redux/timer/timerReducer.js
@@ -1,28 +1,38 @@
+import { createReducer } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
 import { INCREMENT, DECREMENT, CHANGE_STEP } from './timerTypes';
 
-const valueReducer = (state = 0, { type, payload }) => {
-  switch (type) {
-    case INCREMENT:
-      return state + payload;
+// const valueReducer = (state = 0, { type, payload }) => {
+//   switch (type) {
+//     case INCREMENT:
+//       return state + payload;
 
-    case DECREMENT:
-      return state - payload < 0 ? state : state - payload;
+//     case DECREMENT:
+//       return state - payload < 0 ? state : state - payload;
 
-    default:
-      return state;
-  }
-};
+//     default:
+//       return state;
+//   }
+// };
 
-const stepReducer = (state = 5, { type, payload }) => {
-  switch (type) {
-    case CHANGE_STEP:
-      return Number(payload);
+const valueReducer = createReducer(0, {
+  [INCREMENT]: (_, { payload }) => _ + payload,
+  [DECREMENT]: (_, { payload }) => _ - payload,
+});
 
-    default:
-      return state;
-  }
-};
+// const stepReducer = (state = 5, { type, payload }) => {
+//   switch (type) {
+//     case CHANGE_STEP:
+//       return Number(payload);
+
+//     default:
+//       return state;
+//   }
+// };
+
+const stepReducer = createReducer(5, {
+  [CHANGE_STEP]: (_, { payload }) => Number(payload),
+});
 
 export default combineReducers({
   value: valueReducer,
